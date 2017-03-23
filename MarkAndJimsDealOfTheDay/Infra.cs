b25a318daa8b52b85a701ff98a4b3abde071c0c9
jimp <jimp@pageuppeople.com>
2017-03-23 14:05:29 +1100

    Ready for presso

---
+++ MarkAndJimsDealOfTheDay/Infra.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace MarkAndJimsDealOfTheDay
+{
+    public class DomainOperationResult
+    {
+        public IEntity Entity { get; }
+        public IEvent Event { get; }
+
+        public DomainOperationResult(IEntity entity, IEvent evt)
+        {
+            Entity = entity;
+            Event = evt;
+        }
+    }
+
+    public interface IEntity
+    {
+    }
+
+    public interface IEvent
+    {
+    }
+
+    public interface IUow
+    {
+        IRepository Repository { get; set; }
+        IBus Bus { get; set; }
+    }
+
+    public interface IBus
+    {
+        void Publish(IEvent evt);
+    }
+
+    public interface IRepository
+    {
+        T Get<T>(Guid id);
+        void Save<T>(T entity);
+    }
+}
